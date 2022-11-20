99fad3f2fcde18d4840812489fc6616d2ee2725f
l37sg0 <petar_1v@abv.bg>
2022-11-20 12:09:20 +0000

    [#3]  - added backup files script

---
+++ backup_files.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+############################################################################################
+# HELP
+############################################################################################
+Help()
+{
+  # Display Help.
+  echo "Creates zip archive backup from specified directory to specified backup directory."
+  echo
+  echo "Syntax: backup_files.sh [-d] [-bd]"
+  echo "options:"
+  echo "-d    Specifies directory to be backuped."
+  echo "-bd   Specifies backupDirectory for the backup being saved."
+  echo "-h    Prints this help message."
+  echo
+}
+
+############################################################################################
+# EXECUTE
+############################################################################################
+Execute()
+{
+  # Execute the script.
+  zip -r "${backupDirectory}/backup.zip" $directory
+  echo "Done.";
+}
+
+############################################################################################
+# MAIN
+############################################################################################
+
+while getopts d:b:h flag
+do
+  case "${flag}" in
+    d) directory=${OPTARG};;
+    b) backupDirectory=${OPTARG};;
+    h) Help
+  esac
+done
+Execute
